1bfa602cdc41386dde412cf0ac771c6c286fd115
cbluan207206-oss <cbluan207206@gmail.com>
2026-06-09 02:35:57 +0700

    Create products.js

---
+++ products.js
@@ -0,0 +1,18 @@
+const DANH_SACH_GIAY = [
+    {
+        id: 1,
+        name: "Nike Mercurial Vapor 16 Academy TF",
+        price: "1.850.000đ",
+        image: "nike-vapor-16.png",
+        sizes: [39, 40, 41, 42, 43],
+        hot: true
+    },
+    {
+        id: 2,
+        name: "Mizuno Morelia Neo 4 Pro TF",
+        price: "2.350.000đ",
+        image: "mizuno-neo-4.png",
+        sizes: [40, 41, 42, 43, 44],
+        hot: false
+    }
+];
