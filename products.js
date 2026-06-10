3533049ba1c13be9ad09f76d908a6f97e2b0fce9
cbluan207206-oss <cbluan207206@gmail.com>
2026-06-10 19:34:19 +0700

    Update products.js

--- products.js
+++ products.js
@@ -1,53 +1,53 @@
 const DANH_SACH_GIAY = [
     {
         id: 1,
-        name: "Nike Mercurial Vapor 16 Academy TF - Burgundy Edition",
-        brand: "Nike",
-        priceRaw: 1250000,
-        priceSale: 980000,
-        colors: ["Đỏ rượu"],
-        sizes: [39, 40, 41, 42],
-        image: "2_2images.jpeg",
+        name: "Giày Bóng Đá Zocker Winner Energy Gen 2 - Xanh Đen TF",
+        brand: "ZOCKER",
+        priceRaw: ,
+        priceSale: 699000,
+        colors: ["Xanh Đen"],
+        sizes: [39, 40, 41, 42, 43, 44, 45],
+        image: "zocker.jpg",
         images: [
-            "adidas-main.jpg",
-             "adidas-side.jpg",
-             "adidas-sole.jpg"
+            "zocker_1.jpg",
+            "zocker_2.jpg",
+            "zocker_3.jpg"
         ],
-        desc: "Phiên bản đặc biệt phối màu đỏ rượu vang thượng hạng. Cấu trúc Upper dệt sợi cải tiến mang lại cảm giác bóng tối ưu, đệm giảm chấn Air Zoom siêu nhẹ giúp tăng tốc bứt phá trên mặt sân cỏ nhân tạo.",
+        desc: "Giày Bóng Đá Zocker Winner Energy Gen 2 Xanh Đen TF sở hữu thiết kế mạnh mẽ, đế TF bám sân tốt cùng form ôm chân thoải mái. Lựa chọn lý tưởng cho các trận đấu sân cỏ nhân tạo với độ ổn định và cảm giác bóng ấn tượng.",
         featured: true
     },
     {
         id: 2,
-        name: "Mizuno Morelia Neo IV Pro AS - Gold Metallic",
-        brand: "Mizuno",
-        priceRaw: 1750000,
-        priceSale: 1490000,
-        colors: ["Vàng đồng"],
-        sizes: [40, 41, 42, 43],
-        image: "1images.png",
+        name: " Giày Bóng Đá Zocker Winner Energy Gen 2 - Xanh Nhạt TF",
+        brand: "ZOCKER",
+        priceRaw: ,
+        priceSale: 699000,
+        colors: ["Xanh Nhạt"],
+        sizes: [39, 40, 41, 42, 43, 44, 45],
+        image: "zocker1.jpg",
         images: [
-            "adidas-main.jpg",
-             "adidas-side.jpg",
-             "adidas-sole.jpg"
+            "zocker1_1.jpg",
+            "zocker1_2.jpg",
+            "zocker1_3.jpg"
         ],
-        desc: "Đỉnh cao phân khúc giày đá bóng phom chân bè. Chất liệu da thật Kangaroo siêu mềm mại đúc nguyên khối kết hợp đường chỉ dập nổi tinh tế mang đậm phong cách hoàng gia sang trọng.",
+        desc: "Giày Bóng Đá Zocker Winner Energy Gen 2 Xanh Nhạt TF nổi bật với phối màu trẻ trung, hiện đại cùng thiết kế tối ưu cho sân cỏ nhân tạo. Đế TF bám sân hiệu quả, hỗ trợ di chuyển linh hoạt và kiểm soát bóng tự tin trong mọi pha xử lý.",
         featured: true
     },
     {
         id: 3,
-        name: "Nike Mercurial Vapor 16 Elite TF - Pitch Black Stealth",
-        brand: "Nike",
-        priceRaw: 1900000,
-        priceSale: 1650000,
-        colors: ["Đen"],
-        sizes: [39, 40, 41, 42, 43],
-        image: "1images.png",
+        name: "Giày Bóng Đá Việt Nam Superfly 11 Xanh Chuối Gót Bạc TF",
+        brand: "WIMBRO",
+        priceRaw: 420000,
+        priceSale: 169000,
+        colors: ["Xanh Chuối"],
+        sizes: [39, 40, 41, 42, 43, 44, 45],
+        image: "fly11.jpn",
         images: [
-            "adidas-main.jpg",
-             "adidas-side.jpg",
-             "adidas-sole.jpg"
+            "fly11_1.jpg",
+            "fly11_2.jpg",
+            "fly11_3.jpg"
         ],
-        desc: "Sắc đen Matte huyền bí quyền lực. Trọng lượng siêu nhẹ chỉ ~200g, dòng sản phẩm phân cấp Elite tối thượng dành cho các cầu thủ ưa chuộng lối đá kiểm soát tốc độ và dứt điểm nhanh.",
+        desc: "Giày Bóng Đá Việt Nam Superfly 11 Xanh Chuối Gót Bạc TF gây ấn tượng với phối màu nổi bật, năng động và cá tính. Đế TF bám sân tốt, thân giày ôm chân giúp tăng sự ổn định, hỗ trợ bứt tốc và xử lý bóng hiệu quả trên sân cỏ nhân tạo.",
         featured: true
     },
     {
@@ -57,7 +57,7 @@ const DANH_SACH_GIAY = [
         priceRaw: 1100000,
         priceSale: 850000,
         colors: ["Đen"],
-        sizes: [39, 41, 42],
+        sizes: [39, 41, 42, 43, 44, 45],
         image: "1images.png",
         images: [
             "adidas-main.jpg",
@@ -74,7 +74,7 @@ const DANH_SACH_GIAY = [
     priceRaw: 1200000,
     priceSale: 950000,
     colors: ["Đỏ rượu", "Đen"],
-    sizes: [39, 40, 41, 42, 43],
+    sizes: [39, 40, 41, 42, 43, 44, 45],
     image: "vapor17.jpg",
     images: ["vapor17.jpg", "vapor17_1.jpg", "vapor17_2.jpg", "vapor17_3.jpg"],
     desc: "Dòng Vapor kinh điển với thiết kế năng động...",
